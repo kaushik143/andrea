399519491ff68db942a32663439412ba4aad3958
kaushik <kaushik@dream11.com>
2020-10-06 15:19:12 +0530

    POC project

---
+++ app/src/main/java/com/learn/andrea/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.learn.andrea
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var toolbar: ActionBar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toolbar = supportActionBar!!
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.action_android -> {
+                toolbar.title = "Android"
+                val androidFragment = AndroidFragment.newInstance()
+                openFragment(androidFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.action_react -> {
+
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+}
---
+++ app/src/main/java/com/learn/andrea/android.kt
@@ -0,0 +1,17 @@
+package com.learn.andrea
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class AndroidFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.fragment_android, container, false)
+
+    companion object {
+        fun newInstance(): AndroidFragment = AndroidFragment()
+    }
+}
